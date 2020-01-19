2cafce5fea0b5d23999ac9ffd35380480f22b3c9
sahapranta <pranta1204@gmail.com>
2020-01-19 23:10:16 +0600

    first

---
+++ upload.php
@@ -0,0 +1,15 @@
+<?php
+
+// $filename = 'pic_'.date('YmdHis') . '.jpeg';
+$filename = $_POST['name'] . '.jpeg';
+$data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $_POST['data']));
+
+$url = 'upload/' . $filename;
+
+file_put_contents($url, $data);
+
+if (file_exists($url)) {   		
+	echo 'true';
+} else {
+	echo 'false';
+}
